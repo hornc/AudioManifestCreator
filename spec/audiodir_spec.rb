2ed9449391930f637caea371450948f0148a8ee4
Charles Horn <charles.horn@gmail.com>
2015-07-18 23:24:05 +1200

    update audio_dir spec for Rspec 3

--- spec/audiodir_spec.rb
+++ spec/audiodir_spec.rb
@@ -1,84 +1,93 @@
 require_relative '../audio_dir'
 
 describe AudioDir do
+  let(:test_dir)         { AudioDir.new("spec/data", /.mp3$|.wav$/) }
+  let(:without_manifest) { AudioDir.new("spec/data/no_manifest") }
+  let(:with_manifest)    { AudioDir.new("spec/data/has_manifest") }
+  let(:date_time)        { /\d{2}:\d{2}:\d{2},\d{1,2} \w{3} \d{4}/ }
+
   context "directory has no manifest" do
-    audio_dir = AudioDir.new("spec/data/no_manifest")
-    it "should return false for whether it has a csv audio manifest" do
-      audio_dir.should_not have_manifest
-    end
+    subject { without_manifest }
+    it { should_not have_manifest }
     it "should generate and appropriate manifest name based on the directory name" do
-      audio_dir.manifest.should == "spec/data/no_manifest_manifest.csv"
+      expect(subject.manifest).to eq "spec/data/no_manifest_manifest.csv"
     end
   end
+
   context "directory has a manifest" do
-    audio_dir = AudioDir.new("spec/data/has_manifest")
-    it "should return true for whether it has a csv audio manifest" do
-      audio_dir.should have_manifest
-    end
+    subject { with_manifest }
+    it { should have_manifest }
     it "should return the manifest name" do
-      audio_dir.manifest.should == "test_manifest.csv"
+      expect(subject.manifest).to eq "test_manifest.csv"
     end
   end
 
-  it "should list audio file contents (case insensitive) given a set sample_match pattern" do
-    audio_dir = AudioDir.new("spec/data", /.mp3$|.wav$/)
-
-    audio_dir.should have_samples
-
-    audio_dir.samples.should be_kind_of(Array)
-    audio_dir.should have(5).samples
-    audio_dir.samples.each do |s|
-      s.should match(/.mp3$|.wav$|.MP3$|.WAV$/)
+  describe "directory samples" do
+    # should be a list of audio file contents (case insensitive) matching sample_match
+    subject { test_dir }
+    it { should have_samples }
+    it "should have the correct number of samples" do
+      expect(subject.samples.size).to eq 5
     end
-    
-    audio_dir.sample_match = /.doc$/ 
-    audio_dir.should_not have_samples
-
-    audio_dir.sample_match = /.mp3$/ 
-    audio_dir.should have(2).samples
-
-    audio_dir.sample_match = /.mp4$/ 
-    audio_dir.should have(1).samples
+    it "should have samples of the correct type" do
+      subject.samples.each do |s|
+        expect(s).to match /.mp3$|.wav$|.MP3$|.WAV$/
+      end
+    end
+    it "should be empty when pattern does not match" do
+      subject.sample_match = /.doc$/
+      is_expected.to_not have_samples
+    end
+    it "should list case mixed extensions" do
+      subject.sample_match = /.mp3$/
+      expect(subject.samples.size).to eq 2
+    end
+    it "should include .mp4" do
+      subject.sample_match = /.mp4$/
+      expect(subject.samples.size).to eq 1
+    end
   end
 
-  it "should generate a audio manifest csv file given a set sample_match pattern" do
-    audio_dir = AudioDir.new("spec/data", /.mp3$|.wav$/)
-    result = audio_dir.generate_manifest
-    result.should be_kind_of(String)
-    result.should match(/Files located at:/)
-    result.should match(/Recording, Description, Location,/)
-    result.should match(/sample_16bit_01,,,00:03.361,15:44:18,15 Jul 2011,2,16,44100,WAV/)
-    result.should match(/sample02,,,00:03.452,15:42:42,15 Jul 2011,2,128kb\/s,44100,MP3/)   # capitalized MP3 extension
+  describe "generated audio manifest csv" do
+    subject { test_dir.generate_manifest }
+    it { should be_kind_of(String) }
+    it { should match(/Files located at:/) }
+    it { should match(/Recording, Description, Location,/) }
+    it { should match(/sample_16bit_01,,,00:03.361,#{date_time},2,16,44100,WAV/) }
+    it { should match(/sample02,,,00:03.452,#{date_time},2,128kb\/s,44100,MP3/) } # capitalized MP3 extension
   end
 
-  it "should return an array of sample information headers" do
-    audio_dir = AudioDir.new("spec/data")
-    headers = audio_dir.sample_headers
-    headers.should be_kind_of(Array)
-    headers.size.should == 10
-    headers[0].should == "Recording"
-    headers[1].should == "Description"
-    headers[9].should == "Format"
+  describe "header columns of the samples" do
+    subject { test_dir.sample_headers }
+    it "should have the correct number of headers" do
+      expect(subject.size).to eq 10
+    end
+    it "has expected titles and order" do
+      expect(subject[0]).to eq 'Recording'
+      expect(subject[1]).to eq 'Description'
+      expect(subject[9]).to eq 'Format'
+    end
   end
 
-  it "should return an array of hashes containing the sample information for each sample" do
-    audio_dir = AudioDir.new("spec/data", /.mp3$|.wav$/)
-    audio_dir.sample_info.should be_kind_of(Array)
-    audio_dir.sample_info[0].should be_kind_of(Hash)
-    audio_dir.sample_info[0]["Description"].should == "This is a test description 0001"
-    audio_dir.sample_info[0]["Location"].should == "NZ"
-    audio_dir.sample_info[1]["Description"].should == "This is a test description 0002"
-    audio_dir.sample_info[1]["Location"].should == "South Island, NZ"
+  describe "sample information" do
+    subject { test_dir.sample_info }
+    it "should be an array of hashes" do
+      expect(subject[0]).to be_kind_of Hash
+    end
+    it "should expected location and description" do
+      expect(subject[0]["Description"]).to eq "This is a test description 0001"
+      expect(subject[0]["Location"]).to eq "NZ"
+      expect(subject[1]["Description"]).to eq "This is a test description 0002"
+      expect(subject[1]["Location"]).to eq "South Island, NZ"
+    end
   end
 
-  it "should generate and return a .PLS playlist file for all samples it contains" do
-    audio_dir = AudioDir.new("spec/data", /.mp3$|.wav$/)
-    playlist = audio_dir.generate_playlist
-    playlist.should be_kind_of(String)
-    playlist.should match("[playlist]")
-    playlist.should match("File1=")
-    playlist.should match("Title1=")
-    playlist.should match("NumberOfEntries=5")
-    puts playlist
+  describe "generated .PLS playlist" do
+    subject { test_dir.generate_playlist }
+    it { should be_kind_of String }
+    it { should match("[playlist]") }
+    it { should match("File1=") }
+    it { should match("Title1=") }
+    it { should match("NumberOfEntries=5") }
   end
 end
